55ccdb2371aeb609324cca8c3da76ee2396a7099
konvio <v.o.kononenko@gmail.com>
2018-04-24 17:06:06 +0300

    1. Two Sum: Refactor solution

--- src/main/java/io/konv/leetcode/TwoSum.java
+++ src/main/java/io/konv/leetcode/TwoSum.java
@@ -14,16 +14,14 @@ public class TwoSum {
      * You may assume that each input would have exactly one solution, and you may not use the same element twice.
      */
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> hashMap = new HashMap<>();
-        int[] res = new int[2];
+        Map<Integer, Integer> indexMap = new HashMap<>();
         for (int i = 0; i < nums.length; ++i) {
-            if (hashMap.containsKey(target - nums[i])) {
-                res[0] = hashMap.get(target - nums[i]);
-                res[1] = i;
-                return res;
+            if (indexMap.containsKey(target - nums[i])) {
+                int matchingIndex = indexMap.get(target - nums[i]);
+                return new int[]{matchingIndex, i};
             }
-            hashMap.put(nums[i], i);
+            indexMap.put(nums[i], i);
         }
-        return res;
+        return null;
     }
 }
